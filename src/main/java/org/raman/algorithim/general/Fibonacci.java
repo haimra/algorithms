346c15befb0605104509d6f6b86e3eef567071b2
haimra <haimra@gmail.com>
2018-04-11 08:05:38 +0300

    adding oprimized recursive soultion

--- src/main/java/org/raman/algorithim/general/Fibonacci.java
+++ src/main/java/org/raman/algorithim/general/Fibonacci.java
@@ -1,41 +1,63 @@
 package org.raman.algorithim.general;
 
 /**
- * 
  * fib(0) = 0 fib(1) = 1 fib(n) = fn(n-1)+f(n-2) 0,1,1,2,3,5,8,13...
  */
 public class Fibonacci {
 
-	public int recursiveFib(int n) {
-		if (n == 0)
-			return 0;
-		if (n == 1)
-			return 1;
-		return recursiveFib(n - 1) + recursiveFib(n - 2);
-	}
+    /**
+     * Complexity  O(2^n)
+     * Using recursive solution is a very slow
+     * @param n
+     * @return
+     */
+    public long recursiveFib(int n) {
+        System.out.print('.');
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        return recursiveFib(n - 1) + recursiveFib(n - 2);
+    }
 
-	public int iterativeFib(int n) {
-		if (n == 0)
-			return 0;
-		int fn = 0, fnMin1 = 1, i = 0;
-		while (i < n) {
-			fn = fnMin1 + fn;
-			fnMin1 = fn - fnMin1;
-			i++;
-		}
-		return fn;
-	}
+    /**
+      * Iterative solution is is match faster then recursive one
+     *  making the recursive solution irrelevant even for small numbers
+     *
+     * @param n
+     * @return
+     */
+    public long iterativeFib(int n) {
+        if (n == 0)
+            return 0;
+        long fn = 0, fnMin1 = 1, i = 0;
+        while (i < n) {
+            System.out.print('.');
+            fn = fnMin1 + fn;
+            fnMin1 = fn - fnMin1;
+            i++;
+        }
+        return fn;
+    }
 
-	//TODO
-	public int optimizedFib(int n) {
-		if (n == 0)
-			return 0;
-		int fn = 0, fnMin1 = 1, i = 0;
-		while (i < n) {
-			fn = fnMin1 + fn;
-			fnMin1 = fn - fnMin1;
-			i++;
-		}
-		return fn;
-	}
+/**
+ * The following recursive optimization will can be as good the iterative one
+ */
+ public long optimizedFib(int n) {
+        long[] cache = new long[n];
+        return recursiveFib(n, cache);
+    }
+
+    private long recursiveFib(int n, long[] cache) {
+        System.out.print('.');
+        if (n == 0)
+            return 0;
+        if (n == 1) {
+            cache[1] = 1;
+            return 1;
+        }
+        cache[n - 1] = cache[n - 1] != 0 ? cache[n - 1] : recursiveFib(n - 1,cache);
+        cache[n - 2] = cache[n - 2] != 0 ? cache[n - 2] : recursiveFib(n - 2,cache);
+        return cache[n - 1] + cache[n - 2];
+    }
 }
--- src/test/java/org/raman/algorithim/general/FibonacciTest.java
+++ src/test/java/org/raman/algorithim/general/FibonacciTest.java
@@ -7,24 +7,41 @@ import static org.junit.Assert.assertEquals;
 public class FibonacciTest {
 
 	@Test
-	public void testRecorsiveFib() {
+	public void testRecursiveFib() {
+		System.out.println("Test Recursive");
 		Fibonacci fib = new Fibonacci();
-		assertEquals(0,fib.recursiveFib(0));
+		/*assertEquals(0,fib.recursiveFib(0));
 		assertEquals(1,fib.recursiveFib(1));
 		assertEquals(1,fib.recursiveFib(2));
 		assertEquals(2,fib.recursiveFib(3));
-		assertEquals(3,fib.recursiveFib(4));
-		assertEquals(5,fib.recursiveFib(5));
+		assertEquals(3,fib.recursiveFib(4));*/
+		assertEquals(12586269025L,fib.recursiveFib(50));
+		System.out.println("\nDone");
 	}
 
 	@Test
-	public void testiterativeFib() {
+	public void testIterativeFib() {
+		System.out.println("Test Iterative");
 		Fibonacci fib = new Fibonacci();
-		assertEquals(0,fib.iterativeFib(0));
+		/*assertEquals(0,fib.iterativeFib(0));
 		assertEquals(1,fib.iterativeFib(1));
 		assertEquals(1,fib.iterativeFib(2));
 		assertEquals(2,fib.iterativeFib(3));
-		assertEquals(3,fib.iterativeFib(4));
-		assertEquals(5,fib.iterativeFib(5));
+		assertEquals(3,fib.iterativeFib(4));*/
+		assertEquals(12586269025L,fib.iterativeFib(50));
+		System.out.println("\nDone");
+	}
+
+	@Test
+	public void testOptimizedFib() {
+		System.out.println("Test Optimized");
+		Fibonacci fib = new Fibonacci();
+		/*assertEquals(0,fib.optimizedFib(0));
+		assertEquals(1,fib.optimizedFib(1));
+		assertEquals(1,fib.optimizedFib(2));
+		assertEquals(2,fib.optimizedFib(3));
+		assertEquals(3,fib.optimizedFib(4));*/
+		assertEquals(12586269025L,fib.optimizedFib(50));
+		System.out.println("\nDone");
 	}
 }
